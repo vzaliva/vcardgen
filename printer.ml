5a5ba069bc55eb161c5fe0a18f3de23a447e7069
Vadim Zaliva <lord@crocodile.org>
2015-10-29 14:25:59 -0700

    re-indentation

--- printer.ml
+++ printer.ml
@@ -3,9 +3,9 @@ open Vcard_4_0
 let escape_value value =
   let open Core_kernel.Std in
   value |> String.substr_replace_all ~pattern:"\\" ~with_:"\\\\" |> 
-  String.substr_replace_all ~pattern:"\n" ~with_:"\\n" |> 
-  String.substr_replace_all ~pattern:"," ~with_:"\\,"
-  
+    String.substr_replace_all ~pattern:"\n" ~with_:"\\n" |> 
+    String.substr_replace_all ~pattern:"," ~with_:"\\,"
+                              
 let rec split s out =
   match String.length s with
   | l when l <= 75 -> output_string out s
@@ -19,22 +19,22 @@ let print out vcard =
   let open Content_line in
   output_string out "BEGIN:VCARD\r\nVERSION:4.0\r\n";
   vcard.content_lines |> List.iter (fun cl ->
-    let buf = Buffer.create 75 in
-    (match Group.to_string cl.group with 
-    | Some g -> Buffer.add_string buf g; Buffer.add_char buf '.'
-    | None -> ());
-    Name.to_string cl.name |> Buffer.add_string buf;
-    cl.parameters |> List.iter (fun p -> 
-      Buffer.add_char buf ';';
-      Buffer.add_string buf p.Parameter.name;
-      Buffer.add_char buf '=';
-      List.hd p.Parameter.values |> Buffer.add_string buf;
-      List.tl p.Parameter.values |> List.iter (fun v ->
-        Buffer.add_char buf ',';
-        Buffer.add_string buf v)
-      );
-    Buffer.add_char buf ':';
-    Value.to_string cl.value |> escape_value |> Buffer.add_string buf;
-    split (Buffer.contents buf) out;
-    output_string out "\r\n");
+                             let buf = Buffer.create 75 in
+                             (match Group.to_string cl.group with 
+                              | Some g -> Buffer.add_string buf g; Buffer.add_char buf '.'
+                              | None -> ());
+                             Name.to_string cl.name |> Buffer.add_string buf;
+                             cl.parameters |> List.iter (fun p -> 
+                                                  Buffer.add_char buf ';';
+                                                  Buffer.add_string buf p.Parameter.name;
+                                                  Buffer.add_char buf '=';
+                                                  List.hd p.Parameter.values |> Buffer.add_string buf;
+                                                  List.tl p.Parameter.values |> List.iter (fun v ->
+                                                                                    Buffer.add_char buf ',';
+                                                                                    Buffer.add_string buf v)
+                                                );
+                             Buffer.add_char buf ':';
+                             Value.to_string cl.value |> escape_value |> Buffer.add_string buf;
+                             split (Buffer.contents buf) out;
+                             output_string out "\r\n");
   output_string out "END:VCARD\r\n"
